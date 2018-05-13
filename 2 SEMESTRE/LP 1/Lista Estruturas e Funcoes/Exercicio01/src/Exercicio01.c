8aff1948dd3990eada960f7d418d01d29c17eefe
Alexandre Murata <alexandremurata@gmail.com>
2018-05-13 01:28:59 -0300

    Commit inicial. .gitignore + exercicio1

---
+++ 2 SEMESTRE/LP 1/Lista Estruturas e Funcoes/Exercicio01/src/Exercicio01.c
@@ -0,0 +1,38 @@
+/*
+ ============================================================================
+ Name        : Exercicio01.c
+ Author      : alexandre.murata
+ Description : elabore um programa para cadastrar 10 pessoas.
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+	char nome[31];
+	int idade;
+} PESSOA;
+
+
+int main(void) {
+
+	PESSOA person[10];
+	char name[31];
+	int age;
+
+	for(int i = 0 ; i < 10 ; i++) {
+		printf("\nInforme o nome: ");
+		fflush(stdin);
+		scanf("%s", name);
+		strcpy(person[i].nome, name);
+		printf("\nInforme a idade: ");
+		fflush(stdin);
+		scanf("%i", &age);
+		person[i].idade = age;
+
+	}
+	system("pause");
+	return 0;
+}
---
+++ 2 SEMESTRE/LP 1/Lista Estruturas e Funcoes/Exercicio02/src/Exercicio02.c
@@ -0,0 +1,28 @@
+/*
+ ============================================================================
+ Name        : Exercicio02.c
+ Author      : alexandre.murata
+ Version     :
+ Copyright   : 
+ Description : Crie um programa, utilizando um vetor de struct que guarda o
+ 	 	 	   nome, RG e ano de nascimento de,no máximo, 10 pessoas.
+ 	 	 	   O programa permite as seguintes funcionalidades, conforme
+ 	 	 	   escolha do usuário:
+ 	 	 	   a - Cadastrar uma pessoa;
+ 	 	 	   b - Calcular a idade de uma pessoa selecionada pelo usuário;
+ 	 	 	   c - Listar pessoas que nasceram a partir de um determinado ano;
+ 	 	 	   d - Listar pessoas que nasceram antes de um determinado ano;
+ 	 	 	   e- Encontrar o nome de uma pessoa com determinado RG;
+ 	 	 	   s- Sair.
+ 	 	 	   O programa deverá ser executado em loop até o usuário digitar
+ 	 	 	   a opção s. Implemente funções para as opções disponíveis
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts(""); /* prints  */
+	return EXIT_SUCCESS;
+}
